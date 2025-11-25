23fc6413d0c8b2b2523f2aa7380bcbf5431cab65
aakash-gorai <aakashkumargorai@gmail.com>
2025-11-25 22:49:51 +0530

    feat: memento design pattern

---
+++ src/main/java/BehaviourableDesignPatterns/MomentoPattern/CareTaker.java
@@ -0,0 +1,19 @@
+package BehaviourableDesignPatterns.MomentoPattern;
+
+import java.util.Stack;
+
+//manages the memento class (snapshots of the Text Editor)
+public class CareTaker {
+    private final Stack<EditorMemento> history = new Stack<>();
+
+    public void save(TextEditor editor) {
+        history.push(editor.save());
+    }
+
+    public void undo(TextEditor editor) {
+        if(!history.isEmpty()) {
+            history.pop();
+            editor.restore(history.peek());
+        }
+    }
+}
